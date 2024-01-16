cba6f18cec234c95efada8578f125b20545101da
oricode-io <86672229+oricode-io@users.noreply.github.com>
2024-01-16 09:56:08 -0700

    Added routes to get, create, and delete tasks

--- server/server.js
+++ server/server.js
@@ -1,8 +1,46 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const Task = require('./models/Task');
+
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.use(bodyParser.json());
+
+app.get('/tasks', (req, res) => {
+    Task.find()
+        .then(tasks => res.send(tasks))
+        .catch(e => res.status(400).send(e));
+});
+
+app.post('/tasks', (req, res) => {
+    const task = new Task({
+        name: req.body.name,
+    });
+
+    task.save()
+        .then(r => res.send({
+            message: 'Task created successfully',
+            task: r,
+        }))
+        .catch(e => res.status(400).send(e));
+});
+
+app.delete('/tasks/:id', (req, res) => {
+    Task.findByIdAndDelete({_id: req.params.id})
+        .then(r => {
+            if (!r) {
+                return res.status(404).send({
+                    message: 'Task not found',
+                })
+            } else {
+                return res.send({
+                    message: 'Task deleted successfully',
+                });
+            }
+        })
+        .catch(e => res.status(400).send(e));
+});
 
 mongoose.connect(`mongodb://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@database:27017/tasks?authSource=admin`,
     {
