af96c700914b863ebe326be8178c8632fa76488d
oricode-io <86672229+oricode-io@users.noreply.github.com>
2024-01-16 00:47:44 -0700

    Connected server with database

--- server/server.js
+++ server/server.js
@@ -1,6 +1,19 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
-app.listen(8080, () => console.log(`Example app listening on port 8080!`));
+mongoose.connect(`mongodb://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@database:27017/tasks?authSource=admin`,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(r => {
+        console.log('Connected to MongoDB');
+        app.listen(8080, () => console.log('Server is running on port 8080'));
+    })
+    .catch(e => {
+        console.log('Error connecting to MongoDB');
+        console.log(e);
+    });
