c855d5346a006e2d6067810fa15bc3232c35850d
oricode-io <86672229+oricode-io@users.noreply.github.com>
2024-01-16 16:29:08 -0700

    Created `TaskForm` component

---
+++ app/src/components/tasks/TaskForm.tsx
@@ -0,0 +1,30 @@
+import {FC} from "react";
+import TaskInput from "./TaskInput";
+import AddTaskButton from "./AddTaskButton";
+
+interface TaskFormProps {
+    onSubmitHandler: (event: any) => void;
+    enteredTaskText: string;
+    updateTaskTextHandler: (event: any) => void;
+}
+
+const TaskForm: FC<TaskFormProps> = ({onSubmitHandler, enteredTaskText, updateTaskTextHandler}) => {
+    return (
+        <>
+            <h1 className="text-8xl font-bold">Tasks</h1>
+            <form
+                className="flex flex-row justify-center items-center my-10"
+                onSubmit={(event) => onSubmitHandler(event)}
+            >
+                <TaskInput
+                    placeholder={"Feed the turtle..."}
+                    value={enteredTaskText}
+                    onChange={updateTaskTextHandler}
+                />
+                <AddTaskButton onClick={onSubmitHandler}/>
+            </form>
+        </>
+    );
+}
+
+export default TaskForm;
