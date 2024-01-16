cd0e59dc0ec32baedb6b4270832e3199411deeae
oricode-io <86672229+oricode-io@users.noreply.github.com>
2024-01-16 11:08:47 -0700

    Allow same origin header

--- server/server.js
+++ server/server.js
@@ -7,6 +7,13 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
+
 app.get('/tasks', (req, res) => {
     Task.find()
         .then(tasks => res.send(tasks))
