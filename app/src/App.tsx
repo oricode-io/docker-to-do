7efd2d11d63c807a8e06e5f61df23f27d4159b46
oricode-io <86672229+oricode-io@users.noreply.github.com>
2024-01-16 11:12:56 -0700

    Added logic to create, read, and delete tasks.

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,26 +1,84 @@
-import React from 'react';
-import logo from './logo.svg';
+import {useEffect, useState} from 'react';
 import './App.css';
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [tasks, setTasks] = useState([]);
+    const [enteredTaskText, setEnteredTaskText] = useState('');
+
+    const updateTaskTextHandler = (event: any) => {
+        setEnteredTaskText(event.target.value);
+    }
+
+    const onSubmitHandler = async (event: any) => {
+        event.preventDefault();
+        setEnteredTaskText('')
+        try {
+            await createTask(enteredTaskText);
+            await getTasks();
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    async function getTasks() {
+        try {
+            const response = await axios.get("http://localhost:8080/tasks");
+            setTasks(response.data);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    async function createTask(name: string) {
+        try {
+            await axios.post("http://localhost:8080/tasks", {name: name});
+            await getTasks();
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    async function deleteTask(id: number) {
+        try {
+            await axios.delete(`http://localhost:8080/tasks/${id}`);
+            await getTasks();
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    useEffect(() => {
+        (async () => {
+            await getTasks();
+        })();
+    }, []);
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>Tasks</h1>
+                <form onSubmit={(event) => onSubmitHandler(event)}>
+                    <input
+                        type="text"
+                        value={enteredTaskText}
+                        onChange={updateTaskTextHandler}
+                    />
+                    <button type="submit">Create Task</button>
+                </form>
+                <ul>
+                    {tasks.map((task: any) => (
+                        <li key={task._id}>
+                            {task.name}
+                            <button onClick={() => deleteTask(task._id.toString())}>Delete</button>
+                        </li>
+                    ))}
+                </ul>
+            </header>
+        </div>
+    );
+
 }
 
 export default App;
